c0d81e644edb3c28ef9ea6d996011e4014ccacc6
gyanstats <112892065+gyanstats@users.noreply.github.com>
2024-05-15 14:49:25 +0100

    Inserted SIR inside ABC_in_rcpp_openmp.cpp

--- Grace/ABC_in_rcpp_openmp.cpp
+++ Grace/ABC_in_rcpp_openmp.cpp
@@ -5,7 +5,21 @@ using namespace Rcpp;
 
 // Function to simulate data from SIR model
 NumericVector sir(NumericVector theta) {
-  // Implement your SIR model here
+  // Implement SIR model
+  // [[Rcpp::export]]
+  NumericVector SIR(NumericVector s, NumericVector i, NumericVector r, double s0, double i0, double r0, double beta, double gamma) {
+    s[0] = s0;
+    i[0] = i0;
+    r[0] = r0;
+
+    for (int t = 1; t < s.size(); t++) {
+      s[t] = s[t-1] - beta * s[t-1] * i[t-1];
+      i[t] = i[t-1] + beta * s[t-1] * i[t-1] - gamma * i[t-1];
+      r[t] = r[t-1] + gamma * i[t-1];
+    }
+
+    return i;
+  }
 }
 
 // Function to calculate distance
@@ -29,7 +43,7 @@ NumericMatrix abc_algorithm(int n, double eps, int p, NumericVector x, int ncore
     #pragma omp for
     for(int i = 0; i < n; i++) {
       theta_sim = runif(p, 0, 1); // sample from prior
-      x_sim = sir(theta_sim); // simulate data from SIR model
+      x_sim = SIR(theta_sim); // simulate data from SIR model
       dist = calc_dist(x_sim, x); // calculate distance
     
       // Accept-reject step
