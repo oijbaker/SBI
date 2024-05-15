1753a13b0b0b16e208e20a446bf07466448cad01
gyanstats <112892065+gyanstats@users.noreply.github.com>
2024-05-15 13:54:30 +0100

    Update ABC_in_rcpp_openmp.cpp

--- Grace/ABC_in_rcpp_openmp.cpp
+++ Grace/ABC_in_rcpp_openmp.cpp
@@ -15,32 +15,35 @@ double calc_dist(NumericVector x_sim, NumericVector x) {
 
 // [[Rcpp::export]]
 NumericMatrix abc_algorithm(int n, double eps, int p, NumericVector x, int ncores) {
-RMatrix<double> accepted_samples;
-int count = 0;
+  
+  RMatrix<double> accepted_samples;
+  int count = 0;
 
-#pragma omp parallel num_threads(ncores)
-{
-  NumericVector theta_sim(p);
-  NumericVector x_sim(p);
-  double dist;
+  #pragma omp parallel num_threads(ncores)
+  {
+  
+    NumericVector theta_sim(p);
+    NumericVector x_sim(p);
+    double dist;
 
-  #pragma omp for
-  for(int i = 0; i < n; i++) {
-    theta_sim = runif(p, 0, 1); // sample from prior
-    x_sim = sir(theta_sim); // simulate data from SIR model
-    dist = calc_dist(x_sim, x); // calculate distance
+    #pragma omp for
+    for(int i = 0; i < n; i++) {
+      theta_sim = runif(p, 0, 1); // sample from prior
+      x_sim = sir(theta_sim); // simulate data from SIR model
+      dist = calc_dist(x_sim, x); // calculate distance
     
-    // Accept-reject step
-    if(dist <= eps){
-      for(int j = 0; j < p; j++) {
-        accepted_samples(i, j) = theta_sim[j];
-      }
+      // Accept-reject step
+      if(dist <= eps){
+        for(int j = 0; j < p; j++) {
+          accepted_samples(i, j) = theta_sim[j];
+        }
         count++;
       }
     }
-    
-    Rprintf("Acceptance rate: %f\n", (double)count / (i + 1));
-  }
 
+  }
+  
+  Rprintf("Acceptance rate: %f\n", (double)count / (i + 1));
   return accepted_samples;
+  
 }
