230a4cc1f3764d269306c6103cb99939e1532ff7
Ollie Baker <oijbaker@outlook.com>
2024-05-16 13:51:46 +0100

    Got the ABC to run

--- Grace/ABC_in_rcpp_openmp_fix_attempt.cpp
+++ Grace/ABC_in_rcpp_openmp_fix_attempt.cpp
@@ -8,30 +8,44 @@ using namespace Rcpp;
 
 // Function to simulate data from SIR model
 // [[Rcpp::export]]
-NumericVector SIR(NumericVector s, NumericVector i, NumericVector r, double s0, double i0, double r0, double beta, double gamma) {
+NumericVector SIR(double s0, double i0, double r0, double beta, double gamma, int T) {
+  double s[T];
+  double i[T];
+  double r[T];
+  
   s[0] = s0;
   i[0] = i0;
   r[0] = r0;
 
-  for (int t = 1; t < s.size(); t++) {
+  for (int t = 1; t < T; t++) {
     s[t] = s[t-1] - beta * s[t-1] * i[t-1];
     i[t] = i[t-1] + beta * s[t-1] * i[t-1] - gamma * i[t-1];
     r[t] = r[t-1] + gamma * i[t-1];
   }
+  
+  NumericVector i_return;
+  
+  for (int t = 1; t < T; t++) {
+    i_return[t] = i[t];
+  }
 
-  return i;
+  return i_return;
 }
 
 
 // Function to calculate distance
 double calc_dist(NumericVector x_sim, NumericVector x) {
-  // Implement your distance calculation here
+  double total = 0;
+  for (int i=0; i<x.size(); i++) {
+    total += pow(x[i]-x_sim[i], 2);
+  }
+  return total;
 }
 
 // [[Rcpp::export]]
 NumericMatrix ABC(int n, double eps, int p, NumericVector x, int ncores) {
   
-  RcppParallel::RMatrix<double> accepted_samples;
+  NumericMatrix accepted_samples;
   int count = 0;
 
   #pragma omp parallel num_threads(ncores)
@@ -44,7 +58,8 @@ NumericMatrix ABC(int n, double eps, int p, NumericVector x, int ncores) {
     #pragma omp for
     for(int i = 0; i < n; i++) {
       theta_sim = runif(p, 0, 1); // sample from prior
-      x_sim = SIR(rep(0,20), rep(0,20), rep(0,20), 762/763, 1/763, 0, theta_sim[1], theta_sim[2]); // simulate data from SIR model
+      
+      x_sim = SIR(762/763, 1/763, 0, theta_sim[1], theta_sim[2], 20); // simulate data from SIR model
       dist = calc_dist(x_sim, x); // calculate distance
     
       // Accept-reject step
