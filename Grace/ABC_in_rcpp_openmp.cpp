21cd669abdff502218d4dc78f6e8c94d133213fc
gyanstats <112892065+gyanstats@users.noreply.github.com>
2024-05-19 13:27:32 +0100

    Completed code but buggy (when calling SIR)

--- Grace/ABC_in_rcpp_openmp.cpp
+++ Grace/ABC_in_rcpp_openmp.cpp
@@ -1,9 +1,12 @@
 #include <Rcpp.h>
 #include <RcppParallel.h>
 #include <omp.h>
+#include <sitmo.h>
+#include <cmath>
 using namespace Rcpp;
 
 // [[Rcpp::depends(RcppParallel)]]
+// [[Rcpp::depends(sitmo)]]
 // [[Rcpp::plugins(openmp)]]
 
 // Function to simulate data from SIR model
@@ -22,54 +25,72 @@ NumericVector SIR(NumericVector s, NumericVector i, NumericVector r, double s0, 
   return i;
 }
 
+// [[Rcpp::export]]
+double unif_sitmo(int seed) {
+  uint32_t coreseed = static_cast<uint32_t>(seed);
+  sitmo::prng eng(coreseed);
+  double mx = sitmo::prng::max();
+  double x = eng() / mx;
+  return x;
+}
 
 // Function to calculate distance
 // [[Rcpp::export]]
-double calc_dist(NumericVector x_sum, NumericVector x) {
-    if (x_sum.size() != x.size()) {
-        stop("Vectors must be of the same length");
-    }
-    
-    double sum = 0.0;
-    int n = x.size();
+double calc_dist(NumericVector x_sim, NumericVector x) {
+  if (x_sim.size() != x.size()) {
+    stop("Vectors must be of the same length");
+  }
+  
+  double sum = 0.0;
+  int n = x.size();
 
-    #pragma omp parallel for reduction(+:sum)
-    for (int i = 0; i < n; ++i) {
-        sum += std::pow(x_sum[i] - x[i], 2);
-    }
-    
-    return std::sqrt(sum);
+  #pragma omp parallel for reduction(+:sum)
+  for (int i = 0; i < n; ++i) {
+    sum += std::pow(x_sim[i] - x[i], 2);
+  }
+  
+  return std::sqrt(sum);
 }
 
-
 // [[Rcpp::export]]
-NumericMatrix ABC(int n, double eps, int p, NumericVector x, int ncores) {
+NumericMatrix ABC(int n, double eps, int p, NumericVector x, int ncores)
+{
   
-  RcppParallel::RMatrix<double> accepted_samples;
+  NumericMatrix accepted_samples(n, p);
   int count = 0;
+  NumericVector zeros(x.size(), 0);
 
   #pragma omp parallel num_threads(ncores)
   {
-  
     NumericVector theta_sim(p);
-    NumericVector x_sim(p);
-    double dist;
-
-    #pragma omp for
-    for(int i = 0; i < n; i++) {
-      theta_sim = runif(p, 0, 1); // sample from prior
-      x_sim = SIR(rep(0,20), rep(0,20), rep(0,20), 762/763, 1/763, 0, theta_sim[1], theta_sim[2]); // simulate data from SIR model
-      dist = calc_dist(x_sim, x); // calculate distance
+    NumericVector x_sim(x.size());
+    int local_count = 0;
     
+    #pragma omp for
+    for (int i = 0; i < n; i++) {
+      theta_sim[0] = unif_sitmo(i); // sample beta from prior
+      theta_sim[1] = unif_sitmo(i + n); // sample gamma from prior
+      x_sim = SIR(zeros, zeros, zeros, 762.0/763.0, 1.0/763.0, 0, theta_sim[0], theta_sim[1]); // simulate data from SIR model
+      #pragma omp critical
+      {
+      std::cout << "zeros: " << zeros << std::endl;
+      std::cout << "theta_sim[0]: " << theta_sim[0] << std::endl;
+      std::cout << "theta_sum[1]: " << theta_sim[1] << std::endl;
+      std::cout << "x_sim: " << x_sim << std::endl;
+      }
+      double dist = calc_dist(x_sim, x); // calculate distance
+      //std::cout << "dist: " << dist << std::endl;
       // Accept-reject step
-      if(dist <= eps){
-        for(int j = 0; j < p; j++) {
-          accepted_samples(i, j) = theta_sim[j];
+      if (dist <= eps) {
+        #pragma omp critical
+        {
+          for (int j = 0; j < p; j++) {
+            accepted_samples(count, j) = theta_sim[j];
+          }
+          count++;
         }
-        count++;
       }
     }
-
   }
   
   Rprintf("Acceptance rate: %f\n", (double)count / n);
